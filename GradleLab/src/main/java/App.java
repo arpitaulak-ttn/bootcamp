0d9d52b0d5e9c763fb8b27d0f2b6265e2f2f021b
ArpitAulak <arpitaulock@gmail.com>
2020-03-03 11:57:07 +0530

    gradle

---
+++ GradleLab/src/main/java/App.java
@@ -0,0 +1,9 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FilterReader;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello Worlds!");
+    }
+}
